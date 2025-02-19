0912b3bbd0a0575112b9b2357c49e73f9ca8b264
William Wei <128806514+weilycoder@users.noreply.github.com>
2025-02-20 00:34:30 +0800

    测试自动格式化功能 (#3)
    * 测试自动格式化功能
    
    * chore: format code
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ test.py
@@ -0,0 +1,6 @@
+with open("test1.txt", "r") as file1, open("test2.txt", "r") as file2, open(
+        "test3.txt", "r") as file3, open("test4.txt", "r") as file4:
+    print(file1.read())
+    print(file2.read())
+    print(file3.read())
+    print(file4.read())
